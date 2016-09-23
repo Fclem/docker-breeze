4f918e1c8ef249bedef5f197ecbb50e36be8b40d
Clément Fiere <clement.fiere@helsinki.fi>
2016-09-23 20:04:59 +0300

    test

--- my_sql.sh
+++ my_sql.sh
@@ -41,7 +41,7 @@ if hash mysql 2>/dev/null; then
 		docker logs $mysql_cont_name
 		echo -e $RED"INIT FAILURE :"$END_C
 		echo "It seems breeze DB was not loaded into mysql, c.f. container log above"
-		docker rm -f $mysql_cont_name && echo "deleted $mysql_cont_name container"
+		# docker rm -f $mysql_cont_name && echo "deleted $mysql_cont_name container"
 	else
 		echo -e $GREEN"SUCCESS"$END_C
 	fi
