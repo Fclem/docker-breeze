52416e4b7800d84dfb3b18e811be9fc8ca86a0fe
Clément Fiere <clement.fiere@helsinki.fi>
2016-09-23 21:49:07 +0300

    fixing nginx

--- nginx.sh
+++ nginx.sh
@@ -3,8 +3,9 @@
 source run.conf
 
 docker run --name breeze-nginx \
-	-v $local_root_path/nginx/:/etc/nginx/:ro \
-	--restart=on-failure \
+	-v $local_root_path/nginx/:/root:ro \
+	-v $local_root_path/nginx/nginx.conf:/etc/nginx/nginx.conf:ro \
+#	--restart=on-failure \
 	-d -p 443:443 \
 	-p 80:80 \
 	nginx
