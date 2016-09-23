a903eebade553daeac60ef6463224c953005f44f
Clément Fiere <clement.fiere@helsinki.fi>
2016-09-23 17:31:46 +0300

    added some verbosity for debugging

--- my_sql.sh
+++ my_sql.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 source run.conf
 
-docker run \
-	--name $mysql_cont_name \
+run_args="--name $mysql_cont_name \
 	-e MYSQL_ROOT_PASSWORD=$mysql_secret \
 	-v $local_root_path/breeze.sql:/docker-entrypoint-initdb.d/breeze.sql \
 	--restart=on-failure \
-	-d $mysql_image
+	-d $mysql_image"
+
+echo $run_args
+
+docker run $run_args
+#	--name $mysql_cont_name \
+#	-e MYSQL_ROOT_PASSWORD=$mysql_secret \
+#	-v $local_root_path/breeze.sql:/docker-entrypoint-initdb.d/breeze.sql \
+#	--restart=on-failure \
+#	-d $mysql_image
