9109d9c590aef988b0dcba6d6e170021b4b0c235
Clément Fiere <clement.fiere@helsinki.fi>
2016-09-23 22:33:46 +0300

    functional nginx reverse proxy, with static content serving and HTTPS

--- nginx.sh
+++ nginx.sh
@@ -5,6 +5,7 @@ source run.conf
 docker run --name breeze-nginx \
 	-v $local_root_path/nginx/:/root:ro \
 	-v $local_root_path/nginx/nginx.conf:/etc/nginx/nginx.conf:ro \
+	-v $code_folder/static_source/:/www/data/static:ro \
 	-d -p 443:443 \
 	-p 80:80 \
 	nginx
