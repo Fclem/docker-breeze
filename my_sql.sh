869699f917b13f8a30b7e3ea2b7b0d4fe8b2e9b6
Clément Fiere <clement.fiere@helsinki.fi>
2016-09-23 18:58:53 +0300

    test

--- my_sql.sh
+++ my_sql.sh
@@ -8,28 +8,26 @@ docker run --name $mysql_cont_name \
 	-d $mysql_image
 
 mysql_ip=""
+# if mysql CLI exists, we display the list of DATABASE for the user to check if breezedb was successfully created or not
 if hash mysql 2>/dev/null; then
-	# if mysql CLI exists, we display the list of DATABASE for the user to check if breezedb was successfully created or not
-	# mysql -h 172.17.0.2 -u root -p$mysql_secret -e 'SHOW DATABASES;'
+	# wait for container to get its ip adress
 	while [ -z $mysql_ip ]; do
 		echo -ne "waiting for $mysql_cont_name ip adress...\033[0K\r"
 		mysql_ip=`docker inspect $mysql_cont_name | grep IPAddress | grep -v null| cut -d '"' -f 4 | head -1`
 		sleep 1
 	done
-	echo "mysql_ip:"$mysql_ip";"
-
-	secs=$((1 * 60))
+	# wait for mysql init
+	# secs=$((1 * 60))
+	secs=0
 	last_ret=1
-	while [ $secs -gt 0 ] && [ $last_ret -eq 1 ]; do
-	   echo -ne "waiting $secs sec for the initialisation of the database...\033[0K\r"
+	while [ $last_ret -eq 1 ]; do
+	   echo -ne "\r"
+	   echo -ne "waited $secs sec for the initialisation of the database...\033[0K"
 	   sleep 1
-	   : $((secs--))
+	   : $((secs++))
 	   db_list=`mysql -h $mysql_ip -u root -p$mysql_secret -e 'SHOW DATABASES;' 2>/dev/null;`
 	   last_ret=$?
 	done
-	
-	# echo "$db_list"
-	# mysql_ip=`docker inspect $mysql_cont_name | grep IPAddress | grep -v null| cut -d '"' -f 4 | head -1`
-	# echo "mysql_ip:"$mysql_ip";"
+	# output DB list
 	mysql -h $mysql_ip -u root -p$mysql_secret -e 'SHOW DATABASES;'
 fi
