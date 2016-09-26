4779af37334dc813eaf5a83d330a156efa6d8336
Clément Fiere <clement.fiere@helsinki.fi>
2016-09-26 09:33:20 +0300

    disabled HSTS until we get a proprer CERT, enable in docker restart on failure of nginx

--- nginx.sh
+++ nginx.sh
@@ -6,8 +6,8 @@ docker run --name breeze-nginx \
 	-v $local_root_path/nginx/:/root:ro \
 	-v $local_root_path/nginx/nginx.conf:/etc/nginx/nginx.conf:ro \
 	-v $code_folder/static_source/:/www/data/static:ro \
+	--restart=on-failure \
 	-d -p 443:443 \
 	-p 80:80 \
 	nginx
-
-#	--restart=on-failure \
+	
