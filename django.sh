05a2054f3450c9011b5e9f0e8651fe3a1d3fec5f
Clément Fiere <clement.fiere@helsinki.fi>
2016-10-06 12:43:11 +0300

    do not restart breeze on failure

--- django.sh
+++ django.sh
@@ -5,11 +5,11 @@ docker rm -f /$breeze_cont_name
 docker run \
 	-d -ti \
 	--name $breeze_cont_name \
-	--link $mysql_cont_name:mysql \
+	--link $mysql_cont_name:mysql \
 	$fs_param \
 	-p 8000:8000 \
-	--restart=on-failure \
+
 	$full_img_name \
 	$docker_root_folder/isbio/manage.py runserver 0.0.0.0:8000 && exit 0
 exit 1
- 	
+# 	--restart=on-failure \ 	
