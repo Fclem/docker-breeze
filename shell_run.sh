c2f0495e612fa5fb253641991d2a4e9b58f7a524
Clément Fiere <clement.fiere@helsinki.fi>
2016-10-06 14:13:30 +0300

    reflecting changes from run.sh

--- shell_run.sh
+++ shell_run.sh
@@ -6,6 +6,7 @@ docker run \
 	--rm \
 	--name $breeze_cont_name-shell \
 	--link $mysql_cont_name:mysql \
+	--link $mysql_cont_name:$mysql_cont_name \
 	$fs_param \
 	-p 8001:8000 \
 	$full_img_name \
