34c962abec1586b294dff634b4749ccf68dffdb9
Clément Fiere <clement.fiere@helsinki.fi>
2016-10-06 14:16:43 +0300

    update

--- shell_run.sh
+++ shell_run.sh
@@ -5,10 +5,11 @@ docker run \
 	-ti \
 	--rm \
 	--name $breeze_cont_name-shell \
-	--link $mysql_cont_name:mysql \
+	--link $mysql_cont_name:mysql \
 	--link $mysql_cont_name:$mysql_cont_name \
 	$fs_param \
-	-p 8001:8000 \
 	$full_img_name \
 	/usr/bin/fish
+
+# 	-p 8001:8000 \
 
--- stop_all.sh
+++ stop_all.sh
@@ -4,6 +4,6 @@ source run.conf
 
 echo "docker stop $nginx_cont_name $breeze_cont_name $mysql_cont_name"
 docker stop $nginx_cont_name $breeze_cont_name $mysql_cont_name && \
-echo "docker rm $nginx_cont_name $breeze_cont_name $mysql_cont_name" && \
-docker rm $nginx_cont_name $breeze_cont_name $mysql_cont_name && \
+# echo "docker rm $nginx_cont_name $breeze_cont_name $mysql_cont_name" && \
+# docker rm $nginx_cont_name $breeze_cont_name $mysql_cont_name && \
 exit 0
