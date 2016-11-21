1a974e162beeadb071e76877d4ae63df0bf29fe4
Clément Fiere <clement.fiere@helsinki.fi>
2016-11-21 19:57:29 +0200

    updated launch config for shiny

--- start_shiny.sh
+++ start_shiny.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 source run_conf.sh
 
-launch_cmd="-d --name $shiny_cont_name \
+echo -e $L_CYAN"Running Shiny Server $shiny_cont_name"$END_C
+
+echo "Removing any previously running $shiny_cont_name"
+docker rm -f /$shiny_cont_name
+
+all_params="-d --name $shiny_cont_name \
     $shiny_param \
     $shiny_image" # -p 3838:3838
 
-echo -e $SHDOL"docker run "$launch_cmd
 
-docker run $launch_cmd && exit 0
+
+echo -e $SHDOL"docker run $all_params"
+
+docker run $all_params && echo -e $GREEN"SUCCESS"$END_C && exit 0
+echo -e $RED"FAILURE"$END_C
 exit 1
 
